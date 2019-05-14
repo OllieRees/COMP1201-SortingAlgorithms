e458ca7f38b23552aaa57c6a3df2746e182119ab
Ollie Rees (or1g18) <or1g18@soton.ac.uk>
2019-05-14 20:24:22 +0100

    Added some new method interfaces into the header file. Also, added the implementation for bubble sort. Removed radix sort's implementation.

--- sorting.c
+++ sorting.c
@@ -1,11 +1,19 @@
 #include "sorting.h"
 
+void printArray(int arr[], int size) {
+	int i;
+	for(i = 0; i < size; i++) {
+		printf("%d : %d\n ", i, arr[i]);
+	}
+	printf("\n");
+}
+
 /** Two loops so O(n^2) */
 int* selectionSort(int unsorted[], int size) {
 	int remaining = size;
 	int * arr_ptr = unsorted;
 	int i, j, noDone;
-	noDone = 0;
+	noDone = 0; //number of mins. found
 	
 	for(i = 0; i < size; i++) {
 		int min = unsorted[i];
@@ -33,47 +41,41 @@ int* insertionSort(int unsorted[], int size) {
 	int i;
 	//sort the first two
 	if(unsorted[1] < unsorted[0]) {
-		int temp = unsorted[1];
-		unsorted[1] = unsorted[0];
-		unsorted[0] = temp;
+		int temp = unsorted[i-1];
+		unsorted[i-1] = unsorted[i];
+		unsorted[i] = temp;
 	}
 	
 	//sort the second to ith card
 	for(i = 2; i < size; i++) {
 		int j = i;
 		while(unsorted[j] < unsorted[j - 1]) {
-			int temp = unsorted[j];
-			unsorted[j] = unsorted[j - 1];
-			unsorted[j - 1] = temp;
+			int temp = unsorted[j-1];
+			unsorted[j-1] = unsorted[j];
+			unsorted[j] = temp;
 			j--;
 		}
 	}
 	return ptr;
 }
 
-/* Finish implementation */
-int* radixSort(int[] unsorted, int base, int size) {
+int* bubbleSort(int unsorted[], int size) {
 	int* ptr = unsorted;
-	int exp;
-
-	//get max
-	int max = unsorted[0];
-	int i;
-	for(i = 0; i < size; i++) {
-		if(unsorted[i] > max)
-			max = unsorted[i]
-	}
+	int i, j;
 	
-	//cycle from least sig digit to most sig digit
-	while(exp = 0; 0 < max/pow(10, exp); exp++) {
-		int digits[size];
-		for(i = 0; i < size; i++) {
-			digits[i] = unsorted[i]/pow(10, exp);
+	for(i = 0; i < size; i++) {
+		int cardsLeft = size - i;
+		//do one pass
+		for(j = 1; j < cardsLeft; j++) {
+			if(unsorted[j] < unsorted[j-1]) {
+				int temp = unsorted[j-1];
+				unsorted[j-1] = unsorted[j];
+				unsorted[j] = temp;
+			}
 		}
-		for(i = 0; i < size; i++) {
-			insertionSort(digits);
-		}	
+	printArray(unsorted, size);
 	}
+	return ptr;	
 }
 
 int main(int argc, char * argv[]) {
@@ -85,7 +87,7 @@ int main(int argc, char * argv[]) {
 			n--;
 		}
 		int* ptr = malloc(argc * sizeof(int));
-		ptr = insertionSort(arr, argc);
+		ptr = bubbleSort(arr, argc);
 		int i;
 		for(i = 0; i < argc; i++) { 
 			printf("%d\n", ptr[i]);
--- sorting.h
+++ sorting.h
@@ -1,6 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <stdbool.h>
+
+/* Prints the contents of an array */
+void printArray(int arr[], int size);
 
 /* Sorts arr using selection sort */
 int* selectionSort(int unsorted[], int size);
@@ -8,6 +12,16 @@ int* selectionSort(int unsorted[], int size);
 /* Sorts arr using insertion sort. */
 int* insertionSort(int unsorted[], int size);
 
+int* bubbleSort(int unsorted[], int size);
+
+int* shellSort(int unsorted[], int size);
+
+/* Divides the array in half with each iteration; sorts out the numbers in the merge. Divide and Conquer algorithm */
+int* mergeSort(int unsorted[], int size);
+
+/* Similar to mergesort, as it divides the array (use static function?) */
+int* quickSort(int unsorted[], int size);
+
 /* Sorts arr using radix sort */
-int*  radixsort(int unsorted[], int base, int size);
+int*  radixsort(int unsorted[], int size);
 
